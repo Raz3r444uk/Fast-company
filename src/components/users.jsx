ed6ccffe808dfe582d2f061c25a51212776a148d
Гуськов Владимир <raz3r444uk@mail.ru>
2022-07-07 00:20:53 +0400

    fix: added list + create buttons logics

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -4,26 +4,8 @@ import api from "../api";
 const Users = () => {
   const [users, setUsers] = useState(api.users.fetchAll());
 
-  let contOfPeople = api.users.fetchAll().length;
-  const [count, setCounter] = useState(contOfPeople);
-
-  const words = [
-    "человек тусанет с тобой сегодня!",
-    "человека тусанут с тобой сегодня!",
-    "Никто с тобой не тусанет!",
-  ];
-
-  let colorOfTitle = "badge fs-6 ";
-  colorOfTitle += count <= 0 ? "bg-danger" : "bg-primary";
-
-  const titleDynamics = () => {
-    let newCount = Math.abs(count) % 100;
-    let num = count % 10;
-    if (newCount >= 10 && newCount <= 20) return `${count} ${words[0]}`;
-    if (num > 1 && num < 5) return `${count} ${words[1]}`;
-    if (num === 1) return `${count} ${words[0]}`;
-    if (num === 0 || num < 0) return words[2];
-    return `${count} ${words[0]}`;
+  const handleDeleteElement = (elementId) => {
+    setUsers(users.filter((user) => user._id !== elementId));
   };
 
   const nameSurname = "Имя";
@@ -32,55 +14,47 @@ const Users = () => {
   const totalMeetings = "Встретился, раз";
   const mark = "Оценка";
 
-  const hanldeDelete = () => {
-    setCounter(count - 1);
-  };
-
   /* Render elements */
-
-  const tHead = (
-    <thead>
-      {
-        <tr>
-          <th scope="col">{nameSurname}</th>
-          <th scope="col">{quanity}</th>
-          <th scope="col">{proffesiion}</th>
-          <th scope="col">{totalMeetings}</th>
-          <th scope="col">{mark}</th>
-          <th scope="col"></th>
-        </tr>
-      }
-    </thead>
-  );
-
-  const element = (
-    <tr>
-      <th scope="row">{api.users.fetchAll()[0].name}</th>
-      <td>{}</td>
-      <td>{api.users.fetchAll()[0].profession.name}</td>
-      <td>{api.users.fetchAll()[0].completedMeetings}</td>
-      <td>{api.users.fetchAll()[0].rate + "/5"}</td>
-      <td>
-        <button className="btn btn-danger" onClick={hanldeDelete}>
-          Delete
-        </button>
-      </td>
-    </tr>
-  );
 
   return (
     <>
-      <h1 className={colorOfTitle}>{titleDynamics(words)}</h1>
+      <h1>
+        <span></span>
+      </h1>
       <table className="table">
-        {tHead}
+        <thead>
+          <tr>
+            <th scope="col">{nameSurname}</th>
+            <th scope="col">{quanity}</th>
+            <th scope="col">{proffesiion}</th>
+            <th scope="col">{totalMeetings}</th>
+            <th scope="col">{mark}</th>
+            <th scope="col">Удалить</th>
+          </tr>
+        </thead>
         <tbody>
-          {element}
-          {element}
-          {element}
-          {element}
-          {element}
-          {element}
-          {element}
+          {users.map((user) => {
+            return (
+              <tr key={user._id}>
+                <td>{user.name}</td>
+                <td>
+                  {user.qualities.map((el) => (
+                    <span className={"badge m-1 bg-" + el.color} key={el._id}>
+                      {el.name}
+                    </span>
+                  ))}
+                </td>
+                <td>{user.profession.name}</td>
+                <td>{user.completedMeetings}</td>
+                <td>{user.rate + "/5"}</td>
+                <td>
+                  <button className="btn btn-danger" onClick={() => handleDeleteElement(user._id)}>
+                    delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </>
