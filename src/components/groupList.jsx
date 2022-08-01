babfebf93a56703706894029e327b86d7f278003
Гуськов Владимир <raz3r444uk@mail.ru>
2022-08-01 22:43:42 +0400

    feat: need help with errors solve

---
+++ src/components/groupList.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const GroupList = ({
+    items,
+    valueProperty,
+    contentProperty,
+    onItemSelect,
+    selectedItem
+}) => {
+    const isArray = (items) => {
+        return items.map((item) => {
+            return (
+                <li
+                    key={item[valueProperty]}
+                    className={
+                        "list-group-item" +
+                        (item === selectedItem ? " active" : "")
+                    }
+                    role="button"
+                    onClick={() => onItemSelect(item)}
+                >
+                    {item[contentProperty]}
+                </li>
+            );
+        });
+    };
+
+    const isObject = (items) => {
+        return Object.keys(items).map((item) => {
+            return (
+                <li
+                    key={items[item][valueProperty]}
+                    className={
+                        "list-group-item" +
+                        (items[item] === selectedItem ? " active" : "")
+                    }
+                    role="button"
+                    onClick={() => onItemSelect(items[item])}
+                >
+                    {items[item][contentProperty]}
+                </li>
+            );
+        });
+    };
+
+    return (
+        <ul className="list-group">
+            {Array.isArray(items) ? isArray(items) : isObject(items)}
+        </ul>
+    );
+};
+
+GroupList.defaultProps = {
+    valueProperty: "_id",
+    contentProperty: "name"
+};
+
+GroupList.propTypes = {
+    items: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    valueProperty: PropTypes.string.isRequired,
+    contentProperty: PropTypes.string.isRequired,
+    onItemSelect: PropTypes.func,
+    selectedItem: PropTypes.object,
+    isArray: PropTypes.func.isRequired,
+    isObject: PropTypes.func.isRequired
+};
+
+export default GroupList;
